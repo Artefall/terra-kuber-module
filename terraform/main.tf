ebb9c44addaf4cd5d47ac05609365ef171059c2a
Artem Maksymov <Artem.Maksymov@genesys.com>
2021-12-02 22:01:11 +0200

    #14: Add some info to main file

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws"{
+    region = var.region
+}
+
+resource "aws_eks_cluster" "this"{
+    name = "EKS Cluster"
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "EKS Cluster Role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.cluster.name
+}
